e2f5b9bbdb65adcd2fc9ad649d1127a408ccbc40
corentinmagique <corentinckf.pro@gmail.com>
2022-04-19 14:51:40 +0400

    start playlist detail

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,8 @@ $(document).ready(()=>{
     $('.moreBtn').on('click', ()=>{
         $('.moreMenu').toggleClass('d-none')
     })
+
+    $('.movDisp').on('mouseleave', ()=>{
+        $('.moreMenu').addClass('d-none')
+    })
 })
