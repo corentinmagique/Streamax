f860beb303b773d0714476e74f6d979dafe441ee
corentinmagique <corentinckf.pro@gmail.com>
2022-05-03 10:43:52 +0400

    theme version alpha done

--- assets/js/main.js
+++ assets/js/main.js
@@ -366,7 +366,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
                         </div>
                     `))
                     modalBody.appendChild(modalContent)
-                    let stmb_selected_num = 0
+                    var stmb_selected_num = 0
 
                     modalBody.querySelectorAll('.streamable').forEach((el)=>{
                         el.addEventListener('click', ()=>{
@@ -393,6 +393,212 @@ window.addEventListener("DOMContentLoaded", (event) => {
                     modal.querySelector('.modal-header .modal-title').innerText = 'Ajouter une programmation'
                     modalBody.appendChild(modalContent)
                 break;
+                case 'prog-addPl':
+                    modalContent = stringToHtml(`
+                    <div><div class="d-flex widget-title-wrapper flex-row m-3 justify-content-between align-items-center">
+                        <h5 class="text-uppercase widget-title">Mes playlists</h5>
+                        
+                    </div>
+    
+                    <div class="row">
+                        <a href="#" class="col-lg-3 col-md-6 col-12 streamable">
+                            <div class="thumbnail mini">
+                                <img src="../assets/media/img/placeholder/temp_vod2.jpg">
+                                <div class="video-length badge">5 vidéos</div>
+                                <div class="video-hover">
+                                    <span class="material-icons">add</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-2 d-flex justify-content-center align-items-center">
+                                    <img height="32px" class="m-2 author-pic rounded-circle" src="../assets/media/img/placeholder/nixia.png">
+                                </div>
+                                <div class="col-10">
+                                    <div class="d-flex flex-column m-2 thumbnail-infos">
+                                        <span class="name">Playlist1</span>
+                                        <div class="author me-2">
+                                            <span class="material-icons-outlined me-1">person</span>
+                                            <span>Nixia</span>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="views col-6">
+                                                <span class="material-icons-outlined me-1">visibility</span>
+                                                <span>153 vues</span>
+                                            </div>
+                                            <div class="date col-6">
+                                                <span class="material-icons-outlined me-1">calendar_today</span>
+                                                <span>8 mois</span>
+                                            </div>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                    
+                                </div>
+                            </div>
+
+                        </a>
+                        <a href="#" class="col-lg-3 col-md-6 col-12 streamable">
+                            <div class="thumbnail mini">
+                                <img src="../assets/media/img/placeholder/temp_vod4.jpg">
+                                <div class="video-length badge">58 vidéos</div>
+                                <div class="video-hover">
+                                    <span class="material-icons">add</span>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-2 d-flex justify-content-center align-items-center">
+                                    <img height="32px" class="m-2 author-pic rounded-circle" src="../assets/media/img/placeholder/nixia.png">
+                                </div>
+                                <div class="col-10">
+                                    <div class="d-flex flex-column m-2 thumbnail-infos">
+                                        <span class="name">Playlist2</span>
+                                        <div class="author me-2">
+                                            <span class="material-icons-outlined me-1">person</span>
+                                            <span>Nixia</span>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="views col-6">
+                                                <span class="material-icons-outlined me-1">visibility</span>
+                                                <span>153 vues</span>
+                                            </div>
+                                            <div class="date col-6">
+                                                <span class="material-icons-outlined me-1">calendar_today</span>
+                                                <span>8 mois</span>
+                                            </div>
+                                        </div>
+                                        
+                                        
+                                    </div>
+                                    
+                                </div>
+                            </div>
+
+                        </a>
+                    </div></div>
+    
+                    `)
+                    modal.querySelector('.modal-header .modal-title').innerText = 'Ajouter une playlist'
+                    modal.querySelector('.modal-footer').appendChild(stringToHtml(`
+                        <div class="d-flex flex-row justify-content-between align-items-center w-100">
+                            <span class="text-secondary"><span class="selected_num">0</span> élement(s) selectionné(s)</span>
+                            <button class="tamerBtn">Ajouter</button>
+                        </div>
+                    `))
+                    modalBody.appendChild(modalContent)
+                    var stmb_selected_num = 0
+
+                    modalBody.querySelectorAll('.streamable').forEach((el)=>{
+                        el.addEventListener('click', ()=>{
+                            el.classList.toggle('selected')
+                            stmb_selected_num = (el.classList.contains('selected')) ?  ++stmb_selected_num : --stmb_selected_num
+                            modal.querySelector('.selected_num').innerText  = stmb_selected_num
+                        })
+                    })
+                break;
+                case 'prog-addScreen':
+                    modalContent = stringToHtml(`
+                    <div><div class="d-flex widget-title-wrapper flex-row m-3 justify-content-between align-items-center">
+                        <h5 class="text-uppercase widget-title">Mes écrans</h5>
+                        
+                    </div>
+    
+                    <div class="row">
+                        <a href="#" class="col-lg-3 col-md-6 col-12 screen">
+                            <div class=" card device">
+                                <div class="card-body">
+                                    
+                                    <span class="material-icons-outlined text-secondary">
+                                        tv
+                                    </span>
+                                    
+                                </div>
+
+                                <div class="card-footer">
+                                    
+                                    <span class="name">
+                                        Smart TV de Carine
+                                    </span>
+                                    <span class="infos">
+                                        02b:4789::987
+                                    </span>
+                                    
+                                </div>
+                            </div>
+
+                        </a>
+
+                        <a href="#" class="col-lg-3 col-md-6 col-12 screen">
+                            <div class=" card device">
+                                <div class="card-body">
+                                    
+                                    <span class="material-icons-outlined text-secondary">
+                                        smartphone
+                                    </span>
+                                    
+                                </div>
+
+                                <div class="card-footer">
+                                    
+                                    <span class="name">
+                                        Smartphone de Carine
+                                    </span>
+                                    <span class="infos">
+                                        02b:4789::987
+                                    </span>
+                                    
+                                </div>
+                            </div>
+
+                        </a>
+
+                        <a href="#" class="col-lg-3 col-md-6 col-12 screen">
+                            <div class=" card device">
+                                <div class="card-body">
+                                    
+                                    <span class="material-icons-outlined text-secondary">
+                                        laptop
+                                    </span>
+                                    
+                                </div>
+
+                                <div class="card-footer">
+                                    
+                                    <span class="name">
+                                        Ordinateur de Carine
+                                    </span>
+                                    <span class="infos">
+                                        02b:4789::987
+                                    </span>
+                                    
+                                </div>
+                            </div>
+
+                        </a>
+                    </div></div>
+    
+                    `)
+                    modal.querySelector('.modal-header .modal-title').innerText = 'Ajouter un écran'
+                    modal.querySelector('.modal-footer').appendChild(stringToHtml(`
+                        <div class="d-flex flex-row justify-content-between align-items-center w-100">
+                            <span class="text-secondary"><span class="selected_num">0</span> élement(s) selectionné(s)</span>
+                            <button class="tamerBtn">Ajouter</button>
+                        </div>
+                    `))
+                    modalBody.appendChild(modalContent)
+                    var stmb_selected_num = 0
+
+                    modalBody.querySelectorAll('.screen').forEach((el)=>{
+                        el.addEventListener('click', ()=>{
+                            el.classList.toggle('selected')
+                            stmb_selected_num = (el.classList.contains('selected')) ?  ++stmb_selected_num : --stmb_selected_num
+                            modal.querySelector('.selected_num').innerText  = stmb_selected_num
+                        })
+                    })
+                break;
+                   
 
             }
 
@@ -401,14 +607,17 @@ window.addEventListener("DOMContentLoaded", (event) => {
 
             closeBtns.forEach((btn)=>{
                 btn.addEventListener('click', ()=>{
+                    $(modal).modal('hide')
                     removeAllChildNodes(modalBody)
                     removeAllChildNodes(modalFooter)
-                    $(modal).modal('hide')
+                    
                 })
             })
 
         })
     })
+
+    $('.days').select2().val(['Lundi', 'Mardi', 'Dimanche']).trigger('change')
 
     
 })
